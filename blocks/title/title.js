34184c027cf4ba49a6760ec01ba379debd0ebb54
michseixas <mcseixas@gmail.com>
2025-12-10 12:33:15 +0100

    feca-5: final styling of title

--- blocks/title/title.js
+++ blocks/title/title.js
@@ -1,28 +1,29 @@
 export default function decorate(block) {
   // Wrap text after <br> tag in a span for styling
-  const h1Elements = block.querySelectorAll('h1');
-  h1Elements.forEach((h1) => {
-    const br = h1.querySelector('br');
-    if (br) {
-      // Get all text nodes and elements after the <br>
-      const fragment = document.createDocumentFragment();
-      let node = br.nextSibling;
-      while (node) {
-        const nextSibling = node.nextSibling;
-        fragment.appendChild(node);
-        node = nextSibling;
-      }
-      
-      // Only create span if there's content after the br
-      if (fragment.hasChildNodes()) {
-        // Create span wrapper
-        const span = document.createElement('span');
-        span.className = 'title-after-br';
-        span.appendChild(fragment);
-        
-        // Insert span after br
-        br.after(span);
-      }
+  const h2Elements = block.querySelectorAll('h2');
+  h2Elements.forEach((h2) => {
+    // Avoid re-wrapping if decorator runs more than once
+    if (h2.querySelector('.title-after-br')) return;
+
+    // Only use the first <br> if multiple exist
+    const br = h2.querySelector('br');
+    if (!br) return;
+
+    // Collect everything after the chosen <br>
+    const fragment = document.createDocumentFragment();
+    let node = br.nextSibling;
+    while (node) {
+      const nextSibling = node.nextSibling;
+      fragment.appendChild(node);
+      node = nextSibling;
+    }
+
+    // Only create span if there's content after the br
+    if (fragment.hasChildNodes()) {
+      const span = document.createElement('span');
+      span.className = 'title-after-br';
+      span.appendChild(fragment);
+      br.after(span);
     }
   });
 }
